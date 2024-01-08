46fd7164d750cf73ee2aca4fbc6ac11075c7863a
pratikash <pratikash@ncompass.inc>
2024-01-08 07:06:59 +0000

    second commit

---
+++ file2.c
@@ -0,0 +1,6 @@
+this is git tutorial. i am trying to learn git
+.this is git tutorial. i am trying to learn git.this is git tutorial. i am trying to learn git
+.this is git tutorial. i am trying to learn git
+.this is git tutorial. i am trying to learn git
+.this is git tutorial. i am trying to learn git
+.this is git tutorial. i am trying to learn git.
